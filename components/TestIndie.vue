d448bc90fda36367eaed984c9043b6812b963962
hyojchaB <hyojcha@smilegate.com>
2023-02-28 17:58:20 +0900

    feat: indie style

---
+++ components/TestIndie.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <button @click="onClickPlusButton">+</button>
+        <span>{{ number }}</span>
+        <button @click="onClickMinusButton">-</button>
+    </div>
+</template>
+
+<!-- 인디 스타일 -->
+<script lang="ts">
+import { ref, defineComponent } from 'vue';
+
+const counter = () => {
+    const number = ref(1);
+
+    const onClickPlusButton = () => {
+        number.value++;
+    };
+
+    const onClickMinusButton = () => {
+        number.value--;
+    };
+
+    return {
+        number,
+        onClickPlusButton,
+        onClickMinusButton,
+    };
+};
+
+export default defineComponent({
+    name: 'TestIndie',
+    setup() {
+        return {
+            ...counter(),
+        };
+    },
+    mounted() {
+        console.log('[test-indide] mounted');
+    },
+});
+</script>
--- components/TestList.vue
+++ components/TestList.vue
@@ -1,34 +1,21 @@
 <template>
     <div>
-        <div id="counter">
-            <button @click="onClickPlusButton">+</button>
-            <span>{{ number }}</span>
-            <button @click="onClickMinusButton">-</button>
-        </div>
         <div id="children">
             <TestItem :text="props1">test props</TestItem>
             <TestItem>test default props value</TestItem>
+            <test-indie></test-indie>
         </div>
     </div>
 </template>
 
 <!-- <script setup> 장점 : https://mine-it-record.tistory.com/640 -->
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue';
+import { onMounted } from 'vue';
 import TestItem from './TestItem.vue';
-
-const number = ref(1);
+import TestIndie from './TestIndie.vue';
 
 // props
 const props1 = 'props text';
-
-const onClickPlusButton = () => {
-    number.value++;
-};
-
-const onClickMinusButton = () => {
-    number.value--;
-};
 
 onMounted(() => {
     console.log('mounted!');
@@ -36,10 +23,6 @@ onMounted(() => {
 </script>
 
 <style lang="scss" scoped>
-#counter {
-    background-color: beige;
-    border: 1px solid gray;
-}
 #children {
     & > * {
         margin: 10px 0;
