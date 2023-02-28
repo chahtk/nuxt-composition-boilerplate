ebca95d9da262c0010a05b2d767044893fd71e40
hyojchaB <hyojcha@smilegate.com>
2023-02-28 17:14:00 +0900

    feat: hello world

---
+++ components/TestItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <span>
+            <slot></slot>
+        </span>
+        <span>props : {{ props.text }}</span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    text: {
+        type: String,
+        default: 'default text',
+    },
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/TestList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div id="counter">
+            <button @click="onClickPlusButton">+</button>
+            <span>{{ number }}</span>
+            <button @click="onClickMinusButton">-</button>
+        </div>
+        <div id="children">
+            <test-item :text="props1">child1</test-item>
+            <test-item>child2</test-item>
+        </div>
+    </div>
+</template>
+
+<!-- <script setup> 장점 : https://mine-it-record.tistory.com/640 -->
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+
+const number = ref(1);
+
+// props
+const props1 = '123';
+
+const onClickPlusButton = () => {
+    number.value++;
+};
+
+const onClickMinusButton = () => {
+    number.value--;
+};
+
+onMounted(() => {
+    console.log('mounted!');
+});
+</script>
+
+<style lang="scss" scoped>
+#counter {
+    background-color: beige;
+    border: 1px solid gray;
+}
+#children {
+    & > * {
+        margin: 10px 0;
+    }
+}
+</style>
